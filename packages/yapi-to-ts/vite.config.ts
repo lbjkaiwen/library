362d0d92ca980e4f17a02f6bfb217386df912047
bobby <874952398@qq.com>
2025-05-15 01:20:33 +0800

    feat: 文件构建

--- packages/yapi-to-ts/vite.config.ts
+++ packages/yapi-to-ts/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+// vite.config.ts
+import { defineConfig } from "vite";
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, './src/cli.ts'),
+      name: 'yapi2ts',
+      // 构建好的文件名（不包括文件后缀）
+      fileName: 'yapi2ts',
+    },
+    rollupOptions: {
+      external: [
+        'yargs',
+        'fs',
+        'path',
+        'url',
+        'util', // 防止 `inspect` 报错
+        'os',   // 防止可能的 `os` 模块报错
+        'yargs '
+      ],
+    },
+  }
+});
